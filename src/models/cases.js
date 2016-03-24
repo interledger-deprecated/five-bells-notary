9867ecbbc2b123d2e4a1bf0e556654a53cfcebef
Chris Clark <clark@ripple.com>
2016-03-23 18:06:15 -0700

    Fix broken transaction handling and fix PUT idempotence for cases

--- src/models/cases.js
+++ src/models/cases.js
@@ -2,10 +2,8 @@
 
 module.exports = CasesFactory
 
-const co = require('co')
 const CaseFactory = require('./db/case')
 const Log = require('../lib/log')
-const DB = require('../lib/db')
 const config = require('../lib/config')
 const NotificationWorker = require('../lib/notificationWorker')
 const CaseExpiryMonitor = require('../lib/caseExpiryMonitor')
@@ -13,14 +11,14 @@ const Condition = require('five-bells-condition').Condition
 const UnprocessableEntityError = require('five-bells-shared').UnprocessableEntityError
 const UnmetConditionError = require('five-bells-shared').UnmetConditionError
 const NotFoundError = require('five-bells-shared').NotFoundError
-const knex = require('../lib/knex')
+const db = require('./db/cases')
 
-CasesFactory.constitute = [CaseFactory, Log, DB, NotificationWorker, CaseExpiryMonitor]
-function CasesFactory (Case, log, db, notificationWorker, caseExpiryMonitor) {
+CasesFactory.constitute = [CaseFactory, Log, NotificationWorker, CaseExpiryMonitor]
+function CasesFactory (Case, log, notificationWorker, caseExpiryMonitor) {
   return class Cases {
 
     static * getCase (caseId) {
-      const item = yield Case.findById(caseId)
+      const item = yield db.getCase(Case, caseId)
       if (!item) {
         throw new NotFoundError('Case ' + caseId + ' not found')
       }
@@ -32,61 +30,44 @@ function CasesFactory (Case, log, db, notificationWorker, caseExpiryMonitor) {
       caseInstance.state = 'proposed'
 
       if (caseInstance.notaries.length !== 1) {
-        throw new UnprocessableEntityError('The case must contain exactly one notary (this notary)')
-      } else if (caseInstance.notaries[0] !== config.getIn(['server', 'base_uri'])) {
-        throw new UnprocessableEntityError('The notary in the case must match this notary ' +
-            `(expected: "${config.getIn(['server', 'base_uri'])}", actual: '${caseInstance.notaries[0]}')`)
+        throw new UnprocessableEntityError(
+          'The case must contain exactly one notary (this notary)')
+      } else if (caseInstance.notaries[0] !==
+                 config.getIn(['server', 'base_uri'])) {
+        throw new UnprocessableEntityError(
+          'The notary in the case must match this notary ' +
+          `(expected: "${config.getIn(['server', 'base_uri'])}", ` +
+          `actual: '${caseInstance.notaries[0]}')`)
       }
 
-      // Combination of transaction / knex / sqlite3 doesn't seem to work,
-      // So don't use transaction on sqlite3.
-      const dbAccess = function * (transaction) {
-        yield Case.create(caseInstance, {transaction})
-      }
-      if (knex.config.client === 'sqlite3') {
-        yield dbAccess()
-      } else {
-        yield knex.knex.transaction(co.wrap(function * (transaction) {
-          yield dbAccess(transaction)
-        }))
-      }
+      const existed = yield db.upsertCase(Case, caseInstance)
       yield caseExpiryMonitor.watch(caseInstance)
-
-      return caseInstance.getDataExternal()
+      return {caseData: caseInstance.getDataExternal(), existed}
     }
 
     static * fulfillCase (caseId, fulfillment) {
-      const caseInstance = yield Case.findById(caseId)
-      if (!caseInstance) {
-        throw new UnprocessableEntityError('Unknown case ID ' + caseId)
-      } else if (caseInstance.state === 'rejected') {
-        throw new UnprocessableEntityError('Case ' + caseId + ' is already rejected')
-      } else if (!Condition.testFulfillment(caseInstance.execution_condition, fulfillment)) {
-        throw new UnmetConditionError('Invalid exec_cond_fulfillment')
-      }
-
-      caseExpiryMonitor.validateNotExpired(caseInstance)
-
-      if (caseInstance.state !== 'executed') {
-        caseInstance.state = 'executed'
-        caseInstance.exec_cond_fulfillment = fulfillment
-
-        // Combination of transaction / knex / sqlite3 doesn't seem to work,
-        // So don't use transaction on sqlite3.
-        const dbAccess = function * (transaction) {
-          yield caseInstance.save({transaction})
-          yield notificationWorker.queueNotifications(caseInstance, {transaction})
+      return yield db.transaction(function * (transaction) {
+        const caseInstance = yield db.getCase(Case, caseId, {transaction})
+        if (!caseInstance) {
+          throw new UnprocessableEntityError('Unknown case ID ' + caseId)
+        } else if (caseInstance.state === 'rejected') {
+          throw new UnprocessableEntityError(
+            'Case ' + caseId + ' is already rejected')
+        } else if (!Condition.testFulfillment(
+            caseInstance.execution_condition, fulfillment)) {
+          throw new UnmetConditionError('Invalid exec_cond_fulfillment')
         }
-        if (knex.config.client === 'sqlite3') {
-          yield dbAccess()
-        } else {
-          yield knex.knex.transaction(co.wrap(function * (transaction) {
-            yield dbAccess(transaction)
-          }))
-        }
-      }
 
-      return caseInstance.getDataExternal()
+        caseExpiryMonitor.validateNotExpired(caseInstance)
+
+        if (caseInstance.state !== 'executed') {
+          caseInstance.state = 'executed'
+          caseInstance.exec_cond_fulfillment = fulfillment
+          yield db.updateCase(caseInstance, {transaction})
+          yield notificationWorker.queueNotifications(caseInstance, transaction)
+        }
+        return caseInstance.getDataExternal()
+      })
     }
   }
 }
