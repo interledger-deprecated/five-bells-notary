1e633a30fff9f48686da338e0d5bf64c562a26ee
CircleCI <info@circleci.com>
2016-11-10 02:20:18 +0000

    chore: update gh-pages

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,12 +1,12 @@
 define({
   "name": "five-bells-notary",
-  "version": "4.2.0",
+  "version": "4.2.1",
   "description": "Consensus-capable Byzantine fault tolerant cryptographic notary",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-11-10T02:20:11.005Z",
+    "time": "2016-11-10T02:20:17.885Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
